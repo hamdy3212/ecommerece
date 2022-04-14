59cbcd1de367d4fde1df257559ad7c945358ffdd
hamdy <hamdyyousseif32@gmail.com>
2022-04-14 10:31:58 +0200

    finish front-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+import Product from './pages/Product';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Cart from './pages/Cart';
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <>
+      <Cart/>
+    </>
+  )
+
 };
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://cdn.shopify.com/s/files/1/2803/4000/products/1-PU02-163-U_e8ce454b-1d6e-44a3-8fba-e18ed13c826c_1200x.png?v=1642061327",
+      title: "Bracelt",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://pngimg.com/uploads/watches/watches_PNG101423.png",
+      title: "Hand Watch",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "http://pngimg.com/uploads/wallet/wallet_PNG7517.png",
+      title: "Wallet",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://ghazalexpress.com/image/cache/catalog/Stand%20for%20arranging%20accessories-1-550x550.JPG",
+      title: "Bracelts!",
+    },
+    {
+      id: 2,
+      img: "https://www.ogondesigns.com/en/pics/data/slides/visuelMobilesEn/55-760x0.jpg",
+      title: "Wallets",
+    },
+    {
+      id: 3,
+      img: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/affordable-watches-1575670104.jpg",
+      title: "Hand Watches",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
