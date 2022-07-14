ca56594824d572bffe729686d6bae02fbfc8b4a0
hamdy <hamdyyousseif32@gmail.com>
2022-07-14 19:55:49 +0200

    add payment

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import Product from './pages/Product';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Cart from './pages/Cart';
+import Pay from './pages/Pay';
+import Success from './pages/Success';
+import { Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <Cart/>
-    </>
+      <Routes>
+        <Route path="/" element={<Pay />} />
+        <Route path="success" element={<Success />} />
+      </Routes>
   )
 
 };
